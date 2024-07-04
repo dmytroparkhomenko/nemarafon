b1d81aca187ac796c0f4034baf6748fd9d08be1c
Dmytro Parkhomenko <dim.parkhom2007@gmai.com>
2024-07-04 17:00:51 +0200

    fix: small fixes

--- app/components/VideoEmbed.tsx
+++ app/components/VideoEmbed.tsx
@@ -2,7 +2,7 @@ const VideoEmbed = ({ embedId }: any) => (
   <div className="video-responsive">
     <iframe
       width="100%"
-      height="500"
+      height="450"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
