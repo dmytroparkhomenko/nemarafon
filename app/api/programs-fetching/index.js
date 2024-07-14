c25ceaed4aa8e0ebef43653a1184cf090adeebeb
Dmytro Parkhomenko <dim.parkhom2007@gmai.com>
2024-07-14 17:57:40 +0200

    add: WhyMeSection fetching with WPGraphQL added

--- app/api/programs-fetching/index.js
+++ app/api/programs-fetching/index.js
@@ -1,205 +1,167 @@
+async function fetchGraphQL(query) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}?query=${encodeURIComponent(
+        query
+      )}`,
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`GraphQL error: ${response.status}`);
+    }
+
+    const json = await response.json();
+    if (json.errors) {
+      console.error("GraphQL operation failed:", json.errors);
+      throw new Error("GraphQL operation failed");
+    }
+
+    return json.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
 export async function getPrograms() {
   const query = `
-    {
-      posts  {
-        nodes {
-          uri
-          title
-          programFields {
-            programPrice
-            programCardBackground {
-              node {
-                sourceUrl
+      {
+          posts  {
+              nodes {
+                  uri
+                  title
+                  programFields {
+                      programPrice
+                      programCardBackground {
+                          node {
+                              sourceUrl
+                          }
+                      }
+                      programShortDescription
+                      programDescription {
+                          __typename
+                          programDescriptionItem
+                      }
+                      programContent {
+                          __typename
+                          programVideoLink
+                      }
+                  }
               }
-            }
-            programShortDescription
-            programDescription {
-              __typename
-              programDescriptionItem
-            }
-            programContent {
-              __typename
-              programVideoLink
-            }
           }
-        }
       }
-    }
   `;
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}?query=${encodeURIComponent(
-      query
-    )}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      next: {
-        revalidate: 0,
-      },
-    }
-  );
-
-  const { data } = await res.json();
-
+  const data = await fetchGraphQL(query);
   return data.posts.nodes.reverse();
 }
 
 export async function getProgramByURI(uri) {
   const query = `
-    {
-      post (id: "${uri}", idType:URI) {
-        uri
-        title
-        programFields {
-          programPrice
-          programShortDescription
-          programDescription {
-            __typename
-            programDescriptionItem
+      {
+          post (id: "${uri}", idType:URI) {
+              uri
+              title
+              programFields {
+                  programPrice
+                  programShortDescription
+                  programDescription {
+                      __typename
+                      programDescriptionItem
+                  }
+                  programContent {
+                      __typename
+                      programVideoLink
+                  }
+              }
           }
-          programContent {
-            __typename
-            programVideoLink
-          }
-        }
-    }
-    }
+      }
   `;
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}?query=${encodeURIComponent(
-      query
-    )}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      next: {
-        revalidate: 0,
-      },
-    }
-  );
-
-  const { data } = await res.json();
-
+  const data = await fetchGraphQL(query);
   return data.post;
 }
 
 export async function getPostDataForPayment(uri) {
   const query = `
-    {
-      post (id: "${uri}", idType:URI) {
-        title
-        programFields {
-          programPrice
-          programShortDescription
-        }
+      {
+          post (id: "${uri}", idType:URI) {
+              title
+              programFields {
+                  programPrice
+                  programShortDescription
+              }
+          }
       }
-    }
   `;
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}?query=${encodeURIComponent(
-      query
-    )}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      next: {
-        revalidate: 0,
-      },
-    }
-  );
-
-  const { data } = await res.json();
-
+  const data = await fetchGraphQL(query);
   return data.post;
 }
 
 export async function getResults() {
   const query = `
-    {
-      results {
-        resultsFields {
-          studioResultsCollection {
-            resultImage {
-              node {
-                altText
-                sourceUrl
+      {
+          results {
+              resultsFields {
+                  studioResultsCollection {
+                      resultImage {
+                          node {
+                              altText
+                              sourceUrl
+                          }
+                      }
+                  }
+                  homeResultsCollection {
+                      resultImage {
+                          node {
+                              altText
+                              sourceUrl
+                          }
+                      }
+                  }
               }
-            }
           }
-          homeResultsCollection {
-            resultImage {
-              node {
-                altText
-                sourceUrl
-              }
-            }
-          }
-        }
       }
-    }
   `;
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}?query=${encodeURIComponent(
-      query
-    )}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      next: {
-        revalidate: 0,
-      },
-    }
-  );
-
-  const { data } = await res.json();
-
+  const data = await fetchGraphQL(query);
   return data.results.resultsFields;
 }
 
 export async function getReviews() {
   const query = `
+      {
+          reviews {
+              reviewFields {
+                  review {
+                      reviewCustomerName
+                      reviewCustomerProgram
+                      reviewCustomerRate
+                      reviewCustomerReview
+                  }
+              }
+          }
+      }
+  `;
+  const data = await fetchGraphQL(query);
+  return data.reviews.reviewFields;
+}
+
+export async function fetchWhyMeSections() {
+  const query = `
     {
-      reviews {
-        reviewFields {
-          review {
-            reviewCustomerName
-            reviewCustomerProgram
-            reviewCustomerRate
-            reviewCustomerReview
+      whyMeSectionRoot {
+        whyMeSection {
+          whymesectionRepeater {
+            whymesectionRepeaterItem {
+              whymesectionRepeaterItemTitle
+              whymesectionRepeaterItemContent
+            }
           }
         }
       }
-    }
-  `;
+    }`;
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}?query=${encodeURIComponent(
-      query
-    )}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      next: {
-        revalidate: 0,
-      },
-    }
-  );
-
-  const { data } = await res.json();
-
-  return data.reviews.reviewFields;
+  const data = await fetchGraphQL(query);
+  return data.whyMeSectionRoot.whyMeSection.whymesectionRepeater;
 }
